c36853fab52519ff0b905fa35fb88d8908d983c0
willgpennell <wpennell48@gmail.com>
2024-05-31 10:51:08 -0230

    preload configured to have homedir and join

--- preload.js
+++ preload.js
@@ -1,7 +1,13 @@
 const { contextBridge } = require("electron");
+const os = require("os");
+const path = require("path");
 
-contextBridge.exposeInMainWorld("versions", {
-  node: () => process.versions.node,
-  chrome: () => process.versions.chrome,
-  electron: () => process.versions.electron,
+// passes in function homedir to renderer
+contextBridge.exposeInMainWorld("os", {
+  homedir: () => os.homedir(),
+});
+
+// passes in function join to renderer
+contextBridge.exposeInMainWorld("path", {
+  join: (...args) => path.join(...args),
 });
